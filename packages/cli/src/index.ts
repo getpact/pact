227c758386219cac72471de01591d637cff7f6cd
0x471 <0x471@protonmail.com>
2026-05-10 01:50:08 +0100

    style: format cli index

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,7 +5,9 @@ switch (command) {
   case undefined:
   case "--help":
   case "-h":
-    console.log("pact <command>\n\ncommands:\n  init     create a workspace\n  login    sign in\n  whoami   print current identity\n");
+    console.log(
+      "pact <command>\n\ncommands:\n  init     create a workspace\n  login    sign in\n  whoami   print current identity\n",
+    );
     break;
   default:
     console.error(`unknown command: ${command}`);
